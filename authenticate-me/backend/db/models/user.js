cfc7dad92dd09a8bd14b7cbfb997939fc4c3c5b6
ansorokey <ansorokey@gmail.com>
2023-05-08 19:25:55 -0400

    added default scope to user model

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -38,6 +38,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['email', 'hashedPassword', 'createdAt', 'updatedAt']
+      }
+    }
   });
   return User;
 };

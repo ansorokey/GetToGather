34660c3cbc8d878eec50fdb33f8f66021c2979cb
ansorokey <ansorokey@gmail.com>
2023-05-09 10:18:32 -0400

    changed location of options object in Users drop table migration

--- authenticate-me/backend/db/migrations/20230508224711-create-user.js
+++ authenticate-me/backend/db/migrations/20230508224711-create-user.js
@@ -44,7 +44,6 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'Users';
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('Users', options);
   }
 };

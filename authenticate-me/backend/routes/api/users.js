f50429a509642a42b2d9f72931a6b51e9bda7d0d
ansorokey <ansorokey@gmail.com>
2023-05-10 14:48:24 -0400

    added input validation for user signups post requests

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -3,13 +3,37 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation.js');
 
 const router = express.Router();
 
 // Router Route - /api/users
 
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
 // Sign up a new user
-router.post('/', async (req, res) => {
+// Validate signUp is an array of middleware
+// Will catch if any value is falsy or not of proper length
+router.post('/', validateSignup, async (req, res) => {
     const { email, password, username } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({

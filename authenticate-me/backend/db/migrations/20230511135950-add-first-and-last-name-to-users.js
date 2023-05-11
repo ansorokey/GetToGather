df69c3f3bb142f8ee019420a03ad20b9d6b6a1b5
ansorokey <ansorokey@gmail.com>
2023-05-11 10:06:04 -0400

    added migration for firstName and lastName into Users table

---
+++ authenticate-me/backend/db/migrations/20230511135950-add-first-and-last-name-to-users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'firstName', Sequelize.STRING, {
+      allowNull: false
+    });
+
+    await queryInterface.addColumn('Users', 'lastName', Sequelize.STRING, {
+      allowNull: false
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'firstName');
+    await queryInterface.removeColumn('Users', 'lastName');
+  }
+};

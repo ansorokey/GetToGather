824115f9a49d38d2be5caccee22abbbc220f788a
ansorokey <ansorokey@gmail.com>
2023-05-12 10:24:17 -0400

    added options object to firstLastName seeder

--- backend/db/migrations/20230511135950-add-first-and-last-name-to-users.js
+++ backend/db/migrations/20230511135950-add-first-and-last-name-to-users.js
@@ -1,15 +1,20 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('Users', 'firstName', Sequelize.STRING, {
       allowNull: false
-    });
+    }, options);
 
     await queryInterface.addColumn('Users', 'lastName', Sequelize.STRING, {
       allowNull: false
-    });
+    }, options);
   },
 
   async down (queryInterface, Sequelize) {

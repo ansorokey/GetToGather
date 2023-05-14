860d00a4c1b31378d6df7b4931c4a19a6da3a038
ansorokey <ansorokey@gmail.com>
2023-05-14 09:07:16 -0400

    removed included user model on get groups

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -7,11 +7,7 @@ const router = express.Router();
 //Router route - /api/groups
 
 router.get('/', async (req, res) => {
-    const allGroups = await Group.findAll({
-        include: {
-            model: User
-        }
-    });
+    const allGroups = await Group.findAll();
 
     res.json(allGroups);
 });

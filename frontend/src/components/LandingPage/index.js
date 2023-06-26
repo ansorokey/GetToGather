7f729a908f7eb0962e03713c4ce3ea64c932e199
ansorokey <ansorokey@gmail.com>
2023-06-26 18:41:27 -0400

    added more styling to landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -8,7 +8,7 @@ function LandingPage() {
             <div id='section-1'>
                 <div className='s1-header-text'>
                     <h1>Coming together for a common goal.</h1>
-                    <p>Fighting crime? Going on an adventure? A quest to save the world? Do what you love with people who love what <em>you</em> love.</p>
+                    <p className="p-text">Fighting crime? Going on an adventure? A quest to save the world? Do what you love with people who love what <em>you</em> love.</p>
                 </div>
                 <div className='s1-image'>
                     <img className='landing-img' src={hands}/>
@@ -18,25 +18,25 @@ function LandingPage() {
             <div id='section-2'>
                 <div className='s2-header-text'>
                     <h2>How GetToGather Works</h2>
-                    <p>Become part of a crew and find something to do online or in-person. It's free to join!</p>
+                    <p className="p-text">Become part of a crew and find something to do online or in-person. It's free to join!</p>
                 </div>
             </div>
 
             <div id='section-3'>
                 <div className='s3-tile'>
                     <img src="" />
-                    <h3>See all groups</h3>
-                    <p>Lorem Ipsum</p>
+                    <h3 className='s3-tile-header-text'>See all groups</h3>
+                    <p className="p-text">Lorem Ipsum</p>
                 </div>
                 <div className='s3-tile'>
                     <img src="" />
-                    <h3>Find an event</h3>
-                    <p>Lorem Ipsum</p>
+                    <h3 className='s3-tile-header-text'>Find an event</h3>
+                    <p className="p-text">Lorem Ipsum</p>
                 </div>
                 <div className='s3-tile'>
                     <img src="" />
-                    <h3>Start a new Group</h3>
-                    <p>Lorem Ipsum</p>
+                    <h3 className='s3-tile-header-text'>Start a new Group</h3>
+                    <p className="p-text">Lorem Ipsum</p>
                 </div>
             </div>
 

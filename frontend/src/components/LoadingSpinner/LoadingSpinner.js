b28a14761c71b40d6feee7a43fbe99007997cd3c
ansorokey <ansorokey@gmail.com>
2023-07-17 17:16:57 -0400

    added loading animation to my pages

---
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import './LoadingSpinner.css';
+function LoadingSpinner () {
+    return (
+        <div className='spin-ctn'>
+            <h3>Loading...</h3>
+            <i class="fa-solid fa-spinner"></i>
+        </div>
+    )
+}
+
+export default LoadingSpinner;
